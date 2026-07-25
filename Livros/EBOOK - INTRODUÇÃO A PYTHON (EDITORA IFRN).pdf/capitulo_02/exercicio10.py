9c31115cc28b9d90bdb6f9de7b50607da3d0f2b7
unknown <edilsonpere1r4filho@gmail.com>
2026-07-25 15:06:24 -0300

    Adiciona novas atividades

--- Livros/EBOOK - INTRODUÇÃO A PYTHON (EDITORA IFRN).pdf/capitulo_02/exercicio10.py
+++
@@ -1,7 +0,0 @@
-copo1 = "laranja"
-copo2 = "acerola"
-
-copo1 = "acerola"
-copo2 = "laranja"
-
-print (copo1 + "e" + copo2)
--- Livros/EBOOK - INTRODUÇÃO A PYTHON (EDITORA IFRN).pdf/capitulo_02/exercicio_8.py
+++
@@ -1,5 +0,0 @@
-salario_minimo   = float(input("Informe o valor do salário mínimo atual (R$): "))
-salario_usuario  = float(input("Informe o valor do seu salário mensal (R$): "))
-qtd_salarios = salario_minimo / salario_usuario
-
-print(f"Você recebe aproximadamente {qtd_salarios:.2f} salário(s) mínimo(s).")
